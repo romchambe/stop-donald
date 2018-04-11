f0d7b69ac6a74c5b08178e1358ed2ead90091610
romchambe <chamberomain@gmail.com>
2018-04-11 11:55:32 +0200

    change mailer configuration and minor style edits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,11 +65,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "stop-donald_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.perform_caching = false
-
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'stop-donald.herokuapp.com'
+  host = 'stop-donald.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
